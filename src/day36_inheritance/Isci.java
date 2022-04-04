684a5e0d0966a673841cc1da67a16b01458b3720
Aziz <azizekeathn@gmail.com>
2022-04-04 18:11:09 +0200

    day37

--- src/day36_inheritance/Isci.java
+++ src/day36_inheritance/Isci.java
@@ -1,5 +1,9 @@
 package day36_inheritance;
 
-public class Isci {
+public class Isci extends Personel {
+	
+	public int saatUcreti=10;
+	public String departman="Imalathane";
+	public boolean raporluMu=false;
 
 }
